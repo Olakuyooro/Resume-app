28f8f8565b29b7ceb3d0cf399fcc58a76b00246a
_Chi_ <iwuagwuchibuzor@gmail.com>
2023-02-28 12:05:25 +0100

    another commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
         <h3>Kuyooro Praise</h3>
         <h3>Frontend Developer</h3>
       </main>
-      <a href="https://docs.google.com/document/d/e/2PACX-1vSbxdg17koPuG-4f0YGpWJcWjdW9EsG6mSfCgTvLHLAGuIZyk1QwTHVout-ererQigySwhRyb_osuN-/pub" download="">Download Resume</a>
+      <a href="https://drive.google.com/file/d/1tDlzHg9hwMbL3MuCOAtBsJA-77iSXKms/view?usp=sharing" download="">Download Resume</a>
 
     </div>
   );
