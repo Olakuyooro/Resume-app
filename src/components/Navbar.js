ad99e0c8ac4e3bf2e391b3dc6a4528ced218d2c9
Ola Kuyooro <pabloalabanza9@gmail.com>
2023-09-21 05:23:05 +0100

    reumelink

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
         <h3>Kuyooro Praise</h3>
         <h3>Frontend Developer</h3>
       </main>
-      <a href="https://drive.google.com/file/d/1E0BhLm5TpBzWc4H9AQBPETzJsttMbg2W/view?usp=share_link" download="">Download Resume</a>
+      <a href="https://drive.google.com/file/d/1v3enbHqvhaTh8XcuNZmkJUifMu0f15ZN/view?usp=drive_link" download="">Download Resume</a>
 
     </div>
   );
