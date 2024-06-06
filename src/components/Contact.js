41fa69a7b50cc6b735ae394ae06f9697eb1d02b7
OlaKuyooro <pabloalabanza9@gmail.com>
2024-06-06 12:02:10 +0100

    Menu Update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,11 +5,11 @@ import { FaLinkedin } from 'react-icons/fa';
 const Contact = () => {
   return (
     <div className='text-black md:text-white flex flex-col md:flex-row justify-end md:space-x-6 space-y-3 md:space-y-0 md:pr-8'>
-      <div className='flex'>
+      <div className='flex space-x-1'>
         <MdMail className='text-xl'/>
-        <a href = "mailto: pabloalabanza9@gmail.com">mail</a>
+        <a href = "mailto: olatunbosunkuyooro@gmail.com">mail</a>
       </div>
-      <div className='flex'>
+      <div className='flex space-x-1'>
         <FaLinkedin className='text-xl'/>
         <a
           href='https://www.linkedin.com/in/praise-kuyooro-b58135188/'
