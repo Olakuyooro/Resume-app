8d1712fef7fc8e68523b129bdb32902355971a26
OlaKuyooro <pabloalabanza9@gmail.com>
2024-06-24 20:21:18 +0100

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -46,7 +46,7 @@ const Navbar = () => {
               <Contact />
               <a
                 className="hover:underline"
-                href="https://drive.google.com/file/d/133TM3lPc9HEEjMrgPa78wGMRA4WZwwfV/view?usp=sharing"
+                href="https://drive.google.com/file/d/133TM3lPc9HEEjMrgPa78wGMRA4WZwwfV/view?usp=drive_link"
                 download=""
               >
                 Download Resume
